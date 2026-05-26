2bdbfe291b43855183a8f745e8a114567f8c143e
Jyuls <m25210019@tectijuana.edu.mx>
2026-05-26 12:24:08 -0700

    update14

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,17 +1,27 @@
-// Cargar navbar
-fetch('../navbar.html')
+const basePath = window.location.hostname.includes("github.io")
+    ? "/decla"
+    : "";
+
+// Navbar
+fetch(`${basePath}/navbar.html`)
     .then(r => r.text())
     .then(html => {
-        const cont = document.getElementById('navbar-container');
-        if (cont) cont.innerHTML = html;
+        const cont = document.getElementById("navbar-container");
+
+        if (cont) {
+            cont.innerHTML = html;
+        }
     })
     .catch(err => console.error("Error cargando navbar:", err));
 
-// Cargar footer
-fetch('../footer.html')
+// Footer
+fetch(`${basePath}/footer.html`)
     .then(r => r.text())
     .then(html => {
-        const cont = document.getElementById('footer-container');
-        if (cont) cont.innerHTML = html;
+        const cont = document.getElementById("footer-container");
+
+        if (cont) {
+            cont.innerHTML = html;
+        }
     })
     .catch(err => console.error("Error cargando footer:", err));
